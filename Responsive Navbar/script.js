3bc3533e04b9289e0d7ae711f5303acc6b6733eb
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2023-07-06 19:17:19 +0530

    Added files for navbar

---
+++ Responsive Navbar/script.js
@@ -0,0 +1,8 @@
+let x = document.getElementById("burger");
+function myFunction() {
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
